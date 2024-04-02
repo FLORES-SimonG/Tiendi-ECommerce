3441634d6b972e20c0e63e452c8e4d378e5f55be
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-04-03 01:20:10 +0200

    actividad 1 completa

--- front/vite-project/src/App.jsx
+++ front/vite-project/src/App.jsx
@@ -1,17 +1,60 @@
-import styles from "./App.module.css";
 import "normalize.css";
+// import styles from "./App.module.css";
+import styled from 'styled-components';
 
+const Cuadros = styled.div`
+    background-color: red;
+    display: flex;
+    flex-wrap: wrap;
+    align-content: center;
+    justify-content: center;
+`;
+
+const Cuadro = styled.div`
+    background-color: blue;
+    margin: 6px;
+    width: 200px;
+    height: 25vh;
+
+    @media (min-width: 481px) and (max-width: 786px) {
+        width: calc(33.33% - 12px);
+    }
+
+    @media (max-width: 480px) {
+        width: calc(50% - 12px);
+    }
+`;
 function App() {
+  
+  
+  
+  
+  
   return (
     <div>
       <h1>COLUMNAS</h1>
-      <div className={styles.cuadros}>
+
+
+      
+        <Cuadros>
+          
+            <Cuadro />
+            <Cuadro />
+            <Cuadro />
+            <Cuadro />
+            <Cuadro />
+            <Cuadro />
+            <Cuadro />
+            <Cuadro />
+        </Cuadros>
+  
+      {/* <div className={styles.cuadros}>
         <div className={styles.cuadro}>Cuadro 1</div>
         <div className={styles.cuadro}>Cuadro 2</div>
         <div className={styles.cuadro}>Cuadro 3</div>
         
         <div className={styles.cuadro}>Cuadro 4</div>
-      </div>
+      </div> */}
     </div>
   );
 }
