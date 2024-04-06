e3745d225dc91eb455d61568f63a74eb5eb13c5f
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-04-06 22:57:24 +0200

    Modifiqué estilos en el NAV

--- front/proyecto-next-m4/src/components/navbar/Navbar.tsx
+++ front/proyecto-next-m4/src/components/navbar/Navbar.tsx
@@ -2,9 +2,9 @@
 
 function Navbar() {
   return (
-    <nav className="h-16 bg-customColorPrimary flex flex-row justify-between items-center">
-      <img src="firma.png" alt="Tiendi" className="h-12 p-2" />
-      <ul className="flex space-x-4 text-white text-md font-bold">
+    <nav className="h-16 shadow-md bg-customColorPrimary flex flex-row justify-between items-center">
+      <img src="firma.svg" alt="Tiendi" className="h-12 m-1" />
+      <ul className="flex text-white flex-row gap-4  ">
         <li>Todo</li>
         <li>Tablets</li>
         <li>Celulares</li>
@@ -12,7 +12,7 @@ function Navbar() {
         <li>Radios</li>
         <li>Otros</li>
       </ul>
-      <ul className="flex space-x-4 text-white text-sm p-2">
+      <ul className="flex space-x-2 text-white text-sm p-2 items-center">
         <li>Sign in</li>
         <li><img src="carrito.svg" alt="carrito" /></li>
       </ul>
