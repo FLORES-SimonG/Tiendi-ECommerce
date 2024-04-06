bbe643f3605b2e1689e7d828dcd901daa63e1248
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-04-06 16:28:37 +0200

    Adicioné y modifiqué interf

---
+++ front/proyecto-next-m4/src/components/home/Home.jsx
@@ -0,0 +1,15 @@
+
+
+
+const HomeContainer = styled.div`
+background-color: red;`
+
+function Home() {
+  return (
+    <HomeContainer>
+      <h1>Home 2</h1>
+    </HomeContainer>
+  );
+}
+
+export default Home;
