aa357de2fc6b5ce26de77029934bc3e37b8efa5c
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-04-07 15:52:03 +0200

    Agregué Footer con estilo básico

--- front/proyecto-next-m4/src/app/page.tsx
+++ front/proyecto-next-m4/src/app/page.tsx
@@ -3,6 +3,7 @@ import Navbar from '../components/navbar/Navbar'
 // import "tailwindcss/tailwind.css"
 import Link from 'next/link'
 import Product_card from '@/components/product_card/Product_card'
+import Footer from '../components/footer/Footer'
 
 
 
@@ -24,6 +25,8 @@ export default function Home() {
       
       <h1>Home Page</h1>
       <Product_card />
+
+      <Footer/>
 
     </div>
   )
---
+++ front/proyecto-next-m4/src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+'use client';
+import { Container, Text } from './styles';
+
+const Footer: React.FC = () => {
+  return (
+    <Container>
+      <Text>This is a basic footer</Text>
+    </Container>
+  );
+};
+
+export default Footer;
