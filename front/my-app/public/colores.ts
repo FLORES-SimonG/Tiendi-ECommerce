faf2735b2731ea9dbf088b242909bdfcf0baf8e2
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-04-08 12:14:34 +0200

    Modifiqué colores y optimicé estilos porque rompian

--- front/my-app/public/colores.ts
+++ front/my-app/public/colores.ts
@@ -1,4 +1,4 @@
 export const customColorPrimary = "rgb(173, 172, 217)";
 export const customColorSecondary = "rgb(199, 199, 199)";
 export const customColorTertiary = "rgb(204, 108, 228)";
-export const customColorQuaternary = "rgb(226, 92, 0)";
+export const customColorQuaternary = "rgb(226, 92, 85)";
--- front/my-app/src/components/product_card/Product_card.tsx
+++ front/my-app/src/components/product_card/Product_card.tsx
@@ -1,63 +1,8 @@
 'use client';
-import styled from 'styled-components';
 import { BiCartDownload } from "react-icons/bi";
 import { MdFavoriteBorder } from "react-icons/md";
 import { FaRegShareSquare } from "react-icons/fa";
-
-const CardContainer = styled.div`
-  border: 2px solid red;
-  width: 12rem;
-  margin: 0.5rem;
-  border-radius: 0.5rem;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  overflow: hidden;
-  cursor: pointer;
-  transition: box-shadow 0.2s;
-
-  &:hover {
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  }
-`;
-
-const Image = styled.img`
-  max-width: 10rem;
-  margin: 0.5rem;
-`;
-
-const Content = styled.div`
-  padding: 0.5rem;
-`;
-
-const Title = styled.h4`
-  font-weight: bold;
-  font-size: 0.875rem;
-`;
-
-const Price = styled.h5`
-  font-weight: bold;
-  font-size: 0.75rem;
-`;
-
-const Description = styled.p`
-  font-size: 0.75rem;
-`;
-
-const IconContainer = styled.div`
-  display: flex;
-  padding: 0.5rem;
-`;
-
-const Icon = styled.span`
-  background-color: #e2e8f0;
-  border-radius: 9999px;
-  padding: 0.25rem 0.5rem;
-  font-size: 0.75rem;
-  font-weight: bold;
-  margin-right: 0.5rem;
-`;
+import { CardContainer, Content, Icon, IconContainer, Image, Price, Title } from "./styles";
 
 function ProductCard() {
   return (
@@ -68,7 +13,6 @@ function ProductCard() {
           <Title>Xiaomi Example</Title>
           <Price>$19.99</Price>
         </div>
-        <Description>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</Description>
       </Content>
       <IconContainer>
         <Icon><BiCartDownload /></Icon>
