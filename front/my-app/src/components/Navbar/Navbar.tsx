23fc104057ef39354b06df656a2a32f8b5ec2562
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-04-11 21:47:56 +0200

    Modifiqué un estilo

--- front/my-app/src/components/Navbar/Navbar.tsx
+++ front/my-app/src/components/Navbar/Navbar.tsx
@@ -6,14 +6,17 @@ function Navbar() {
   return (
     <NavbarContainer>
       <NavbarList>
-        <Link href="/">
+        {/* <Link href="/">
+          <NavbarListItem>Bienvenida</NavbarListItem>
+        </Link> */}
+        <Link href="/home">
           <NavbarListItem>Home</NavbarListItem>
         </Link>
         <Link href="/register">
           <NavbarListItem>Registrate</NavbarListItem>
         </Link>
         <Link href="/store">
-          <NavbarListItemSpecial>Productos</NavbarListItemSpecial>
+          <NavbarListItem>Productos</NavbarListItem>
         </Link>
         <Link href="/login">
           <NavbarListItem>Ingresa</NavbarListItem>
