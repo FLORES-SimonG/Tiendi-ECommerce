63fcee00eff99a9fece540409a1b689cf8168501
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-04-11 12:26:39 +0200

    Modificación de estilos en tarjetas

--- front/my-app/src/components/product_card/Product_card.tsx
+++ front/my-app/src/components/product_card/Product_card.tsx
@@ -4,14 +4,14 @@ import { FaRegShareSquare } from "react-icons/fa";
 
 function Product_card(producto: any): any {
   return (
-    <div className="border-2 border-customColorPrimary w-64 h-72 m-2 rounded-lg flex flex-col justify-center items-center overflow-hidden cursor-pointer transition duration-200 ease-in-out transform hover:shadow-lg hover:bg-customColorPrimary">
+    <div className="border-2 border-customColorPrimary w-52 h-72 m-2 rounded-lg flex flex-col justify-center items-center overflow-hidden cursor-pointer transition duration-200 ease-in-out transform hover:shadow-lg hover:bg-customColorPrimary">
       <img
         src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTMkJMLIOX_xX6RMnnf5Wh_ctvqPSvvEW1dpU8vfyLUpditTJq9eMKzGPn3vObZwrbvkiX1UIOYnk6Vte_ydbGBcrrqAPX0BhXY6uTlc8Bl3ZrN3W-KeG8NMg"
         alt={producto.data.name}
         className="w-36 h-36 rounded-full object-cover"
       />
       <div className="p-2 text-center">
-        <h4 className="text-lg font-bold">{producto.data.name}</h4>
+        <h4 className="text-lg font-bold w-50">{producto.data.name}</h4>
         <h5 className="text-sm font-semibold">${producto.data.price}</h5>
       </div>
       <div className="flex justify-center space-x-3">
