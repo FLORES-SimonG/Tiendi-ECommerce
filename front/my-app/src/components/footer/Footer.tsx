d6c972cb84488f1062f4cb065f6712224933a79f
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-04-11 13:40:46 +0200

    Pág not-found creada

--- front/my-app/src/components/footer/Footer.tsx
+++ front/my-app/src/components/footer/Footer.tsx
@@ -13,9 +13,10 @@ function Footer() {
     <div className="flex justify-center">
       <a href="#!" className="me-6 [&>svg]:h-4 [&>svg]:w-4">
         <svg
-          xmlns="http://www.w3.org/2000/svg"
+          //xmlns="http://www.w3.org/2000/svg"
           fill="currentColor"
-          viewBox="0 0 320 512">
+          viewBox="0 0 320 512"> 
+           
          
           <path
             d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
@@ -23,7 +24,7 @@ function Footer() {
       </a>
       <a href="#!" className="me-6 [&>svg]:h-4 [&>svg]:w-4 ">
         <svg
-          xmlns="http://www.w3.org/2000/svg"
+          //xmlns="http://www.w3.org/2000/svg"
           fill="currentColor"
           viewBox="0 0 512 512">
           
@@ -33,7 +34,7 @@ function Footer() {
       </a>
       <a href="#!" className="me-6 [&>svg]:h-4 [&>svg]:w-4">
         <svg
-          xmlns="http://www.w3.org/2000/svg"
+          //xmlns="http://www.w3.org/2000/svg"
           fill="currentColor"
           viewBox="0 0 488 512">
          
@@ -43,7 +44,7 @@ function Footer() {
       </a>
       <a href="#!" className="me-6 [&>svg]:h-4 [&>svg]:w-4">
         <svg
-          xmlns="http://www.w3.org/2000/svg"
+          //xmlns="http://www.w3.org/2000/svg"
           fill="currentColor"
           viewBox="0 0 448 512">
         
@@ -53,7 +54,7 @@ function Footer() {
       </a>
       <a href="#!" className="me-6 [&>svg]:h-4 [&>svg]:w-4">
         <svg
-          xmlns="http://www.w3.org/2000/svg"
+          //xmlns="http://www.w3.org/2000/svg"
           fill="currentColor"
           viewBox="0 0 448 512">
          
@@ -63,7 +64,7 @@ function Footer() {
       </a>
       <a href="#!" className="[&>svg]:h-4 [&>svg]:w-4">
         <svg
-          xmlns="http://www.w3.org/2000/svg"
+          //xmlns="http://www.w3.org/2000/svg"
           fill="currentColor"
           viewBox="0 0 496 512">
         
@@ -83,7 +84,7 @@ function Footer() {
           className="mb-4 flex items-center justify-center font-semibold uppercase md:justify-start">
           <span className="me-3 [&>svg]:h-4 [&>svg]:w-4">
             <svg
-              xmlns="http://www.w3.org/2000/svg"
+              //xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="currentColor">
               <path
@@ -143,7 +144,7 @@ function Footer() {
         <p className="mb-4 flex items-center justify-center md:justify-start">
           <span className="me-3 [&>svg]:h-5 [&>svg]:w-5">
             <svg
-              xmlns="http://www.w3.org/2000/svg"
+              //xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="currentColor">
               <path
@@ -157,7 +158,7 @@ function Footer() {
         <p className="mb-4 flex items-center justify-center md:justify-start">
           <span className="me-3 [&>svg]:h-5 [&>svg]:w-5">
             <svg
-              xmlns="http://www.w3.org/2000/svg"
+              //xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="currentColor">
               <path
@@ -171,13 +172,14 @@ function Footer() {
         <p className="mb-4 flex items-center justify-center md:justify-start">
           <span className="me-3 [&>svg]:h-5 [&>svg]:w-5">
             <svg
-              xmlns="http://www.w3.org/2000/svg"
+              //xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="currentColor">
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd" // fillRule es para rellenar el icono
                 d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
-                clip-rule="evenodd" />
+                clipRule="evenodd"  // clipRule es para recortar el icono
+                />
             </svg>
           </span>
           + 54 9 11 2345 6789
@@ -185,13 +187,13 @@ function Footer() {
         <p className="flex items-center justify-center md:justify-start">
           <span className="me-3 [&>svg]:h-5 [&>svg]:w-5">
             <svg
-              xmlns="http://www.w3.org/2000/svg"
+              //xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="currentColor">
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M7.875 1.5C6.839 1.5 6 2.34 6 3.375v2.99c-.426.053-.851.11-1.274.174-1.454.218-2.476 1.483-2.476 2.917v6.294a3 3 0 003 3h.27l-.155 1.705A1.875 1.875 0 007.232 22.5h9.536a1.875 1.875 0 001.867-2.045l-.155-1.705h.27a3 3 0 003-3V9.456c0-1.434-1.022-2.7-2.476-2.917A48.716 48.716 0 0018 6.366V3.375c0-1.036-.84-1.875-1.875-1.875h-8.25zM16.5 6.205v-2.83A.375.375 0 0016.125 3h-8.25a.375.375 0 00-.375.375v2.83a49.353 49.353 0 019 0zm-.217 8.265c.178.018.317.16.333.337l.526 5.784a.375.375 0 01-.374.409H7.232a.375.375 0 01-.374-.409l.526-5.784a.373.373 0 01.333-.337 41.741 41.741 0 018.566 0zm.967-3.97a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H18a.75.75 0 01-.75-.75V10.5zM15 9.75a.75.75 0 00-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 00.75-.75V10.5a.75.75 0 00-.75-.75H15z"
-                clip-rule="evenodd" />
+                clipRule="evenodd" />
             </svg>
           </span>
           + 49 1575 1234 5678
