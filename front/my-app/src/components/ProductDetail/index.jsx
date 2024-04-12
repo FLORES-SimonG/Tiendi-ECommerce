aaab80af43dd10b25fd8041b991cc38ef9d85ac5
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-04-12 11:25:28 +0200

    Corregí un problema que rompía mis estilos

--- front/my-app/src/components/ProductDetail/index.jsx
+++ front/my-app/src/components/ProductDetail/index.jsx
@@ -2,7 +2,7 @@
 import { IoIosCloseCircleOutline } from "react-icons/io";
 import { useContext } from "react";
 import { ShoppingCartContext } from "../../Context/index.jsx";
-import "./styles.css";
+
 import Image from 'next/image';
 import { imagenDeEjemplo } from "../../../public/variablesGlobales";
 
@@ -15,23 +15,24 @@ const ProductDetail = ()=>{
     console.log(context.productToShow)
     return(
         <aside 
-        className={`${context.isProductDetailOpen? 'flex':'hidden'} detalleProductoIndividual 
-        m-4 w-80 flex flex-col fixed right-0
+        className={`${context.isProductDetailOpen? 'flex':'hidden'}  
+        m-4 w-80 flex flex-col fixed right-0 
         bg-gradient-to-b from-customColorPrimary from-10%  via-white via-50% to-customColorPrimary to-100%
         z-10  border-black border
-         rounded-lg`}>
-           <div className="flex justify-between items-center p-6  ">
+         h-fit
+         rounded-lg
+        `}>
+           <div className="flex justify-between items-center p-4  ">
                <h2 className="text-2xl font-bold text-white">Product Detail</h2>
                <IoIosCloseCircleOutline className="text-white text-3xl transition-colors  font-bold cursor-pointer hover:text-customColorTertiary" onClick={()=>{context.closeProductDetail()}}/>
            </div>
            <hr className="border-white"/>
            <div className="flex flex-col items-center p-6">
-               {/* <Image src={context.productToShow.image} width={300} height={300} alt="imagen" className="w-64 h-64 object-cover"/> */}
-               <img src={imagenDeEjemplo} alt="imagen" className="w-64 h-64 object-cover"/>
-               <h3 className="text-xl font-bold text-white">{context.productToShow.name}</h3>
-               <p className="text-white">{context.productToShow.description}</p>
-               <p className="text-white font-bold">$100</p>
-               <button className="bg-customColorSecondary text-white p-2 rounded-lg">Add to Cart</button>
+              
+               <img src={imagenDeEjemplo} alt="imagen" className=" w-48 h-48 object-cover rounded-xl"/>
+               <h3 className="text-xl font-bold text-black m-2">{context.productToShow.name}</h3>
+               <p className="text-black text-balance  text-sm">{context.productToShow.description}</p>
+               
            </div>
 
         </aside>
