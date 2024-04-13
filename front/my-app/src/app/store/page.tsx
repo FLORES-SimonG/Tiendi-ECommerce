f7e1d14b34671d7047c713323d692ad5b809e3ca
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-04-13 13:38:17 +0200

    Comienzo a crear un filter por categoria

--- front/my-app/src/app/store/page.tsx
+++ front/my-app/src/app/store/page.tsx
@@ -6,6 +6,7 @@ import Products_cards from "@/components/products_cards/Procucts_cards";
 import ProductDetail from "../../components/ProductDetail/index";
 // import MenuCheckoutProductsDetail from "../../components/MenuCheckoutProductsDetail/index";
 import PreFooter from "@/components/PreFooter/PreFooter";
+import Filter from "@/components/Filter/Filter";
 
 // import Carousel from "@/components/Carousel/Carousel";
 
@@ -15,7 +16,7 @@ export default function Store() {
       <Header />
       <ProductDetail />
       <Navbar />
-      {/* <Carousel /> */}
+      <Filter/>
       <h2>Store</h2>
       <Products_cards />
 
---
+++ front/my-app/src/components/Filter/Filter.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+
+
+
+
+export default function Filter() {
+    return (
+        <>
+            <h3 className="bg-red-500"> ESTE ES EL FILTER FUTURO</h3>
+        </>
+    );
+    }
+    
+
+
+
+
+
+
