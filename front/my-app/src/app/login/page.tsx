848b523ce4022fa47fc24fba5669511ede76c556
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-04-17 02:14:08 +0200

    Login Creado sin funcionalidades

--- front/my-app/src/app/login/page.tsx
+++ front/my-app/src/app/login/page.tsx
@@ -1,4 +1,5 @@
-'use client';
+"use client";
+"use client";
 import BurgerMenu from "@/components/BurgerMenu";
 import Footer from "@/components/footer/Footer";
 import Header from "@/components/Header/Header";
@@ -10,14 +11,71 @@ export default function Login() {
   const context = useContext(ShoppingCartContext);
 
   console.log("este es el context=>", context);
-  // console.log("este es el context=>", context);
-    return (
-        <div className="font-sans">
-      <Header/>
-      <BurgerMenu/>
-      <Navbar/>
-        <h1 className="text-sm text-red-500">Login</h1>
-        {/* <Footer/> */}
+
+  return (
+    <div className="font-sans">
+      <Header />
+      <BurgerMenu />
+      <Navbar />
+      <div className=" flex flex-row items-center content-center justify-center my-6">
+        <div
+          className="
+          bg-gradient-to-b from-customColorPrimary from-10% via-customColorPrimary via-25% to-transparent 
+          rounded-lg shadow-xl overflow-hidden"
+        >
+          <div className="pt-8 px-8">
+            <h2 className="text-center text-3xl font-bold text-white">
+              ¡ Hola de nuevo !
+            </h2>
+
+            <form className="mt-8 space-y-6">
+              <div className="rounded-md shadow-sm">
+                <div>
+                  <input
+                    placeholder="Tu Correo Electronico"
+                    className="  w-full px-3 py-3 rounded-md bg-slate-50 focus:outline-none focus:bg-white transition-colors sm:text-sm"
+                    required
+                    
+                    type="email"
+                    name="email"
+                    id="email"
+                  />
+                </div>
+                <div className="mt-4">
+                  <input
+                    placeholder="Contraseña"
+                    className="w-full px-3 py-3 borde bg-slate-50 focus:outline-none focus:bg-white transition-colors rounded-md  sm:text-sm"
+                    required
+                    
+                    type="password"
+                    name="password"
+                    id="password"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <button
+                  className="group relative w-full flex justify-center py-3 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-customColorPrimary hover:scale-110 transition-all"
+                  type="submit"
+                >
+                  Ingresa
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="px-8 py-4 text-center">
+            <span className="text-gray-400">No tienes usuario? </span>
+            <a
+              className="font-medium text-indigo-500 hover:text-indigo-400"
+              href="#"
+            >
+              Regístrate 😎
+            </a>
+          </div>
         </div>
-    );
-    }
+      </div>
+      <Footer />
+    </div>
+  );
+}
