cb955a2f41c96757ad683c416be95df6671fce1b
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-04-21 20:18:28 +0200

    He limpiado import que no se utilizan

--- front/my-app/src/app/store/[...category]/page.tsx
+++ front/my-app/src/app/store/[...category]/page.tsx
@@ -2,28 +2,26 @@
 import { ShoppingCartContext } from "@/Context";
 import Image from "next/image";
 import BurgerMenu from "@/components/BurgerMenu";
-import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import Link from "next/link";
 
-import Footer from "@/components/footer/Footer";
 import { useContext } from "react";
 import { imagenDeEjemplo } from "../../../../public/variablesGlobales";
-import Error from "../../Error"
-import PreFooter from "@/components/PreFooter";
-
-// interface CategoryProps {
-//     params:{category: string;}
-//   }
+import Error from "../../Error";
 
 export default function Category(props: any) {
   const { category } = props.params;
+  console.log("category", category)
 
   const context = useContext(ShoppingCartContext);
   const detalleIndividualDelProucto = context.productData;
   const llaverDelDetalle = Object.keys(detalleIndividualDelProucto);
   if (llaverDelDetalle.length === 0) {
-    return <div><Error/></div>;
+    return (
+      <div>
+        <Error />
+      </div>
+    );
   }
 
   const paraCheckout = () => {
@@ -57,17 +55,13 @@ export default function Category(props: any) {
 
   return (
     <div>
-      {/* <Header /> */}
       <Navbar />
       <BurgerMenu />
-      
 
-
-      {/* <h2>Categoria Dinámica: {category}</h2> */}
       <div className="flex  flex-row flex-nowrap content-center justify-center items-center my-12  ">
         <div className="relative  w-80 flex-row flex-nowrap content-center justify-center items-center rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
           <div className="relative  mx-4 -mt-6 h-40 flex flex-row items-center overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg bg-gradient-to-r from-customColorPrimary via-customColorPrimary to-gray-300">
-            <Image 
+            <Image
               width={600}
               height={600}
               className="object-cover w-40 h-40  mr-3 rounded-full p-2"
@@ -111,8 +105,6 @@ export default function Category(props: any) {
           </div>
         </div>
       </div>
-      {/* <PreFooter /> */}
-      {/* <Footer /> */}
     </div>
   );
 }
