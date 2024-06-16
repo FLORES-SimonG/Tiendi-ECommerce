8a7da4ae665c16d7952f889b8c6c1b027fe4f280
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-06-16 13:01:46 +0200

    elimine la ext del nextconfig

--- front/my-app/next.config.mjs
+++ front/my-app/next.config.mjs
@@ -3,9 +3,6 @@ const nextConfig = {
     images: {
         domains: ['images.pexels.com', 'apple.com','img.icons8.com', 'media.istockphoto.com'],
     },
-    experimental: {
-        optimizePackageImports: ['react-icons'],
-      },
 }
 
 export default nextConfig;
